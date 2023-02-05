ee4335e125797cc979f148e92e72941743b91771
Suraj Yadav <ysuraj877@gmail.com>
2023-02-05 13:10:12 +0530

    Added another test

--- Soundex/main.cpp
+++ Soundex/main.cpp
@@ -10,14 +10,31 @@ int main(int argc, char **argv)
 
 class Soundex
 {
-  public:
-  std::string encode(std::string word){
-    return "";
-  }
+public:
+  std::string encode(std::string word)
+  {
+    std::string result = "";
+    for (int i = 0; i < word.size(); ++i)
+    {
+      if ((word[i] >= 'a' && word[i] <= 'z') || (word[i] >= 'A' && word[i] <= 'Z'))
+      {
+        result = result + word[i];
+      }
+    }
+    return result;
+  };
 };
+
+TEST(Soundex, IgnoresAllCharactersExceptAlphabets)
+{
+  Soundex soundex;
+  auto encoded = soundex.encode("A.#*");
+  ASSERT_THAT(encoded, ::testing::Eq("A"));
+}
 
 TEST(Soundex, RetainsSoleLetterOfOneLetterWord)
 {
   Soundex soundex;
   auto encoded = soundex.encode("A");
+  ASSERT_THAT(encoded, ::testing::Eq("A"));
 }
